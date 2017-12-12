2afb4666b5b5cf864b85d974def7dd4b27a4ec1a
Ricky Tsao <Ricky_Tsao@emap.com>
2017-12-12 16:11:46 +0800

    first commit

---
+++ selectionSort.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// GIVEN
+// E A D H B
+
+// first pass looks for the minimal value, and swaps it with the value at the front of the list
+// A E D H B
+
+// next pass finds the minimal value after the first element (which is now in place) and swaps it.
+// so from e, d, h, b..b is smallest and swap with the next value, which is e.
+// A B D H E
+
+// move on, and do another pass , D, H, E.
+// D is smallest so everything stays
+// A B D H E
+
+// move on, and do another pass. H, E.
+// E is smaller is swap H and E.
+// A B D E H
+
+var dataArray = [8,3,1,6,2];
+
+function swap(array, index1, index2) {
+  var temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+}
+
+// given a certain pass
+// we get the minimum value and swap it
+function swapSmallerValuesToFront(data, pass, lastIndex) {
+  for (var index = pass + 1; index <= lastIndex; index++) {
+      console.log("");
+      console.log("inner index: " + index + ", data[inner]: " + data[index]);
+      console.log("data[min]: " + data[pass]);
+
+      if (data[index] < data[pass]) {
+          console.log(" We need to swap because current element is smaller");
+          swap(data, index, pass);
+          console.log("---> data display: " + data);
+      }
+  }
+}
+
+function selectionSort(intArray) {
+
+    var data = intArray.slice(0);
+    var indexOfMinValue, temp;
+    var lastIndex = data.length-1;
+
+    // we always do length - 1 passes
+    for (var pass = 0; pass <= lastIndex - 1; pass++) {
+      console.log("---------------> Outer Index: " + pass + " <-----------");
+      swapSmallerValuesToFront(data, pass, lastIndex);
+    }
+
+    return data;
+}
+
+let sortedArray = selectionSort(dataArray);
+console.log(sortedArray);
